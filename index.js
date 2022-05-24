b23a3fd6e936d3188e4f76bb17954966f8531e02
skozmi <skozmi@gmail.com>
2022-05-24 14:00:18 +0200

    final project

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 app.get('/', (req, res, next)=>{
     res.render('main');
